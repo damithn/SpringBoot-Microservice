a20a3ca53caf714f15a452b4ac23604477d481dd
damithn <damithnsamarakoon@gmail.com>
2020-05-19 12:43:17 +0530

    firsrt commit - configServer

---
+++ SpringCloudConfigServer-local/src/main/java/com/damithtech/SpringCloudConfigServerlocal/SpringCloudConfigServerLocalApplication.java
@@ -0,0 +1,13 @@
+package com.damithtech.SpringCloudConfigServerlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerLocalApplication.class, args);
+	}
+
+}
---
+++ SpringCloudConfigServer-local/src/test/java/com/damithtech/SpringCloudConfigServerlocal/SpringCloudConfigServerLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damithtech.SpringCloudConfigServerlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
