1dd4fdc2813e38e6003a520671e2a0865929e883
damithn <damithnsamarakoon@gmail.com>
2020-05-19 14:16:13 +0530

    Adding config server - git

---
+++ SpringCloudConfigServer-git/src/main/java/com/damithtech/SpringCloudConfigServergit/SpringCloudConfigServerGitApplication.java
@@ -0,0 +1,13 @@
+package com.damithtech.SpringCloudConfigServergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerGitApplication.class, args);
+	}
+
+}
---
+++ SpringCloudConfigServer-git/src/test/java/com/damithtech/SpringCloudConfigServergit/SpringCloudConfigServerGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damithtech.SpringCloudConfigServergit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
