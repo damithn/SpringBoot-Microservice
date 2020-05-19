a69fd835027a97049894858b795ad48814591c50
damithn <damithnsamarakoon@gmail.com>
2020-05-19 14:57:45 +0530

    Adding Configuration store

--- SpringCloudConfigServer-git/src/main/java/com/damithtech/SpringCloudConfigServergit/SpringCloudConfigServerGitApplication.java
+++ SpringCloudConfigServer-git/src/main/java/com/damithtech/SpringCloudConfigServergit/SpringCloudConfigServerGitApplication.java
@@ -1,9 +1,11 @@
 package com.damithtech.SpringCloudConfigServergit;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerGitApplication {
 
 	public static void main(String[] args) {
